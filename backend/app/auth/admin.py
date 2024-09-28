fb77e4721ffe223f529852e122d892d8f513e63a
doubledare704 <doubledare704@gmail.com>
2024-09-28 21:30:50 +0300

    bump lib versions, changed validate admin method

--- backend/app/auth/admin.py
+++ backend/app/auth/admin.py
@@ -2,14 +2,17 @@ from fastapi import HTTPException, Depends, status
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 from passlib.context import CryptContext
 
-from app.repositories.admin import admin_collection
+from app.repositories.admin import AdminRepository
 
 security = HTTPBasic()
 hash_helper = CryptContext(schemes=["bcrypt"])
 
 
-async def validate_login(credentials: HTTPBasicCredentials = Depends(security)):
-    admin = admin_collection.find_one({"email": credentials.username})
+async def validate_login(
+        credentials: HTTPBasicCredentials = Depends(security),
+        admin_repo: AdminRepository = Depends(AdminRepository)
+):
+    admin = await admin_repo.get_admin(credentials.username)
     if admin:
         password = hash_helper.verify(credentials.password, admin["password"])
         if not password:
